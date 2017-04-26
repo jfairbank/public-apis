a78050aad00d9d52e1f2cf9ba3688160b348eff9
Jeremy Fairbank <elpapapollo@gmail.com>
2017-04-26 17:01:16 -0400

    Require nonempty api values and valid urls

--- src/Data/Api.elm
+++ src/Data/Api.elm
@@ -1,7 +1,8 @@
-module Data.Api exposing (..)
+module Data.Api exposing (Api, apiDecoder)
 
 import Json.Decode.Pipeline exposing (decode, required)
-import Json.Decode exposing (Decoder, string)
+import Json.Decode exposing (Decoder, andThen, map, string, succeed, fail)
+import Regex exposing (Regex, regex)
 
 
 type alias Api =
@@ -14,12 +15,48 @@ type alias Api =
     }
 
 
+urlRegex : Regex
+urlRegex =
+    regex "https?://.*?\\.[a-zA-Z]+"
+
+
+isUrl : String -> Bool
+isUrl =
+    Regex.contains urlRegex
+
+
+validateNotEmpty : String -> Decoder String
+validateNotEmpty value =
+    if String.trim value == "" then
+        fail "Empty value"
+    else
+        succeed value
+
+
+validateUrl : String -> Decoder String
+validateUrl value =
+    if isUrl value then
+        succeed value
+    else
+        fail "Not a URL"
+
+
+nonEmptyString : Decoder String
+nonEmptyString =
+    string |> andThen validateNotEmpty
+
+
+url : Decoder String
+url =
+    nonEmptyString |> andThen validateUrl
+
+
 apiDecoder : Decoder Api
 apiDecoder =
     decode Api
-        |> required "name" string
-        |> required "category" string
-        |> required "description" string
-        |> required "auth" string
-        |> required "https" string
-        |> required "link" string
+        |> required "name" nonEmptyString
+        |> required "category" nonEmptyString
+        |> required "description" nonEmptyString
+        |> required "auth" nonEmptyString
+        |> required "https" nonEmptyString
+        |> required "link" url
